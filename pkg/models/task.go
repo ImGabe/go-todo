d6e3976ecbdc5843b5bf171a193a2d4f7e2ebd3d
ImGabe <gabrielpmonte@hotmail.com>
2021-04-24 13:34:53 -0300

    update models struct

--- pkg/models/task.go
+++ pkg/models/task.go
@@ -1,7 +1,25 @@
 package models
 
+import (
+	"errors"
+	"net/http"
+)
+
 type Task struct {
 	ID          int64  `db:"id" json:"id"`
 	Description string `db:"description" json:"description"`
 	Done        bool   `db:"done" json:"done"`
+}
+
+func (t *Task) Bind(r *http.Request) error {
+	if t.Description == "" {
+		return errors.New("missing required Description fields")
+	}
+
+	return nil
+
+}
+
+func (t *Task) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
 }
